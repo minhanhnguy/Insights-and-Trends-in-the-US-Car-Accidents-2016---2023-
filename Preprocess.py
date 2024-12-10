ac8d98d3c59db958aaadb5032d3403c92f5ad081
minhanhnguy <nguy0335@vandals.uidaho.edu>
2024-12-10 02:41:54 -0800

    updated data

--- Preprocess.py
+++ Preprocess.py
@@ -1,5 +1,19 @@
 import pandas as pd
 
-data = pd.read_csv("US_Accidents_March23.csv")
+#data = pd.read_csv("US_Accidents_March23.csv")
 
-data.head(100).to_csv("sample.csv", index=True)
+#data.head(100).to_csv("sample.csv", index=True)
+
+data_sample = pd.read_csv("US_Accidents_March23.csv")
+
+states = {}
+
+for index, row in data_sample.iterrows():
+    if row["Start_Time"][0:4] == "2023":
+        if row["State"] not in states:
+            states[row["State"]] = 1
+        else:
+            states[row["State"]] += 1
+
+print(states)
+    
